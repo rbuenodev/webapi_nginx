93eee9409df5e00b164c2e4105d02463fb9f72d5
richard augusto bueno <richard_bueno@ymail.com>
2023-02-01 13:51:44 -0300

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController: ControllerBase
+    {        
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            return Ok("Ok");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID([FromRoute] string id)
+        {
+            return Ok(id);
+        }
+
+
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] string id)
+        {
+            return Ok(id);
+        }
+    }
+}
